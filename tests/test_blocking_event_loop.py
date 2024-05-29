a68dd66fe628f87ea04cc4df583d0997d61e3fa8
Huy-DNA <huydo862003@gmail.com>
2024-05-30 03:31:56 +0700

    test: add basic test for blocking event loop

--- tests/test_blocking_event_loop.py
+++ tests/test_blocking_event_loop.py
@@ -1,7 +1,21 @@
+from contextlib import redirect_stdout
+import io
+
 from pasync.runner import Runner
 
 async def hello_async():
     print("Hello async!")
 
-with Runner() as runner:
-    runner.run(hello_async())
+async def bye_async():
+    print("Bye async!")
+
+async def greeting_async():
+    await hello_async()
+    await bye_async()
+
+def test():
+    with Runner() as runner, redirect_stdout(io.StringIO()) as f:
+        runner.run(hello_async())
+        runner.run(bye_async())
+        runner.run(greeting_async())
+    assert f.getvalue() == "Hello async!\nBye async!\nHello async!\nBye async!\n"
