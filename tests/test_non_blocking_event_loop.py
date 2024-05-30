2f81188b6c876e9010e51e32e17633307334bf01
Huy-DNA <huydo862003@gmail.com>
2024-05-30 22:03:07 +0700

    test: rename method call

--- tests/test_non_blocking_event_loop.py
+++ tests/test_non_blocking_event_loop.py
@@ -12,6 +12,6 @@ def test_simple():
         await bye_async()
 
     with Runner(non_blocking = True) as runner:
-        runner.run(hello_async())
-        runner.run(bye_async())
-        runner.run(greeting_async())
+        runner.run_all(hello_async())
+        runner.run_all(bye_async())
+        runner.run_all(greeting_async())
