ab46ba352cb0ed72f5a3434a20e6e36b61736cef
Huy-DNA <huydo862003@gmail.com>
2024-05-30 22:02:24 +0700

    feat: support return values for blocking event loops

--- pasync/runner/__init__.py
+++ pasync/runner/__init__.py
@@ -1,5 +1,6 @@
 from typing import Awaitable
 from pasync.event_loop import EventLoop
+from pasync.task import Task as _Task
 
 
 class Runner():
@@ -20,24 +21,28 @@ class Runner():
             self.__event_loop.signal_stop()
             self.__event_loop.thread.join()
 
-    def run(self, awaitable: Awaitable, *awaitables: Awaitable):
+    def run_all(self, *awaitables: Awaitable):
+        tasks = [_Task(awaitable) for awaitable in awaitables]
         if self.__non_blocking:
-            self.__run_non_blocking(awaitable, *awaitables)
+            self.__run_non_blocking(*tasks)
         else:
-            self.__run_blocking(awaitable, *awaitables)
+            self.__run_blocking(*tasks)
+            return [task.last_result for task in tasks]
 
-    def run_one_task(self, awaitable: Awaitable):
+    def run_task(self, awaitable: Awaitable):
+        task = _Task(awaitable)
         if self.__non_blocking:
-            self.__run_non_blocking(awaitable)
+            self.__run_non_blocking(task)
         else:
-            self.__run_non_blocking(awaitable)
+            self.__run_non_blocking(task)
+            return task.last_result
         
-    def __run_blocking(self, *awaitables: Awaitable):
-        [self.__event_loop.queue(task) for task in awaitables]
+    def __run_blocking(self, *tasks: _Task):
+        [self.__event_loop.queue(task) for task in tasks]
         self.__event_loop.run_blocking()
 
-    def __run_non_blocking(self, *awaitables: Awaitable):
-        [self.__event_loop.queue(task) for task in awaitables]
+    def __run_non_blocking(self, *tasks: _Task):
+        [self.__event_loop.queue(task) for task in tasks]
         if not self.__is_non_blocking_running:
             self.__event_loop.run_non_blocking()
             self.__is_non_blocking_running = True 
