4483b025478debc429cc3e582b6b9975de8d3ad6
Huy-DNA <huydo862003@gmail.com>
2024-05-31 02:04:20 +0700

    test: basic test for promise

--- tests/test_promise.py
+++ tests/test_promise.py
@@ -7,15 +7,11 @@ from pasync import Promise
 
 def test_simple():
     def promise_callback(resolve, reject):
-        def delayed_call():
-            print("about to resolve!")
-            resolve(True)
-
-        print("start promise, waiting 3 seconds")
-        threading.Timer(3, delayed_call, None, None)
-        print("waiting for promise end")
+        print("start promise")
+        print("end promise")
     
     with Runner() as runner, redirect_stdout(io.StringIO()) as f:
         promise = Promise(promise_callback)
         runner.run_task(promise)
-    assert f.getvalue() == "start promise, waiting 3 seconds\nwaiting for promise end\n"
+    assert f.getvalue() == "start promise\nend promise\n"
+
